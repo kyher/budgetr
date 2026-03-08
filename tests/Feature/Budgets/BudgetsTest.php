2e81e0410994ee677c039d8e88aaa0e487b15ade
kyher <kyle.heron@pm.me>
2026-03-08 15:06:43 +0000

    Split out tests / improvements

--- tests/Feature/Budgets/BudgetsTest.php
+++ tests/Feature/Budgets/BudgetsTest.php
@@ -3,9 +3,7 @@
 namespace Tests\Feature\Budgets;
 
 use App\Models\Budget;
-use App\Models\Item;
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -38,103 +36,6 @@ class BudgetsTest extends TestCase
         $this->actingAs($user);
         $response = $this->get(route('budgets.show', ['budget' => $budget->getKey()]));
         $response->assertForbidden();
-    }
-
-    public function test_budget_owner_can_add_items()
-    {
-        $user = User::factory()->create();
-        $budget = Budget::factory()->create(['user_id' => $user->id]);
-
-        $this->actingAs($user);
-        $itemName = fake()->lexify('?????');
-        $response = $this->post(route('budgets.items.add', ['budget' => $budget->getKey()]), [
-            'name' => $itemName,
-            'amount' => 100,
-        ]);
-
-        $response->assertRedirect(route('budgets.show', ['budget' => $budget->getKey()]));
-        $this->assertDatabaseHas('items', [
-            'name' => $itemName,
-            'amount' => 100,
-            'budget_id' => $budget->getKey(),
-        ]);
-    }
-
-    public function test_different_user_cannot_add_items()
-    {
-        $user = User::factory()->create();
-        $budget = Budget::factory()->create();
-
-        $this->actingAs($user);
-        $response = $this->post(route('budgets.items.add', ['budget' => $budget->getKey()]), [
-            'name' => 'New Item',
-            'amount' => 100,
-        ]);
-
-        $response->assertForbidden();
-    }
-
-    public function test_budget_owner_can_remove_items()
-    {
-        $user = User::factory()->create();
-        $budget = Budget::factory()->create(['user_id' => $user->id]);
-        $item = Item::factory()->create(['budget_id' => $budget->id]);
-
-        $this->actingAs($user);
-        $response = $this->delete(route('budgets.items.remove', ['budget' => $budget->getKey(), 'item' => $item->getKey()]));
-
-        $response->assertRedirect(route('budgets.show', ['budget' => $budget->getKey()]));
-        $this->assertDatabaseMissing('items', [
-            'id' => $item->id,
-        ]);
-    }
-
-    public function test_different_user_cannot_remove_items()
-    {
-        $user = User::factory()->create();
-        $budget = Budget::factory()->create();
-        $item = Item::factory()->create(['budget_id' => $budget->id]);
-
-        $this->actingAs($user);
-        $response = $this->delete(route('budgets.items.remove', ['budget' => $budget->getKey(), 'item' => $item->getKey()]));
-
-        $response->assertForbidden();
-        $this->assertDatabaseHas('items', [
-            'id' => $item->id,
-        ]);
-    }
-
-    public function test_budget_owner_can_toggle_item_completion()
-    {
-        Carbon::setTestNow();
-        $user = User::factory()->create();
-        $budget = Budget::factory()->create(['user_id' => $user->id]);
-        $item = Item::factory()->create(['budget_id' => $budget->id, 'paid_at' => null]);
-
-        $this->actingAs($user);
-        $response = $this->patch(route('budgets.items.toggle-paid', ['budget' => $budget->getKey(), 'item' => $item->getKey()]));
-
-        $response->assertRedirect(route('budgets.show', ['budget' => $budget->getKey()]));
-        $this->assertDatabaseHas('items', [
-            'id' => $item->id,
-            'paid_at' => now(),
-        ]);
-    }
-
-    public function test_different_user_cannot_toggle_item_completion()
-    {
-        $user = User::factory()->create();
-        $budget = Budget::factory()->create();
-        $item = Item::factory()->create(['budget_id' => $budget->id, 'paid_at' => null]);
-
-        $this->actingAs($user);
-        $response = $this->patch(route('budgets.items.toggle-paid', ['budget' => $budget->getKey(), 'item' => $item->getKey()]));
-
-        $response->assertForbidden();
-        $this->assertDatabaseHas('items', [
-            'id' => $item->id,
-            'paid_at' => null,
-        ]);
     }
 
     public function test_a_user_can_add_a_budget()
@@ -176,49 +77,5 @@ class BudgetsTest extends TestCase
         $response = $this->delete(route('budgets.remove', ['budget' => $budget->getKey()]));
 
         $response->assertForbidden();
-    }
-
-    public function test_a_user_can_edit_a_budget_item()
-    {
-        $user = User::factory()->create();
-        $budget = Budget::factory()->create(['user_id' => $user->id]);
-        $item = Item::factory()->create(['budget_id' => $budget->id]);
-
-        $this->actingAs($user);
-        $response = $this->patch(route('budgets.items.update', ['budget' => $budget->getKey(), 'item' => $item->getKey()]), [
-            'name' => 'Updated Item',
-            'amount' => 150,
-            'remaining' => 100,
-        ]);
-
-        $response->assertRedirect(route('budgets.show', ['budget' => $budget->getKey()]));
-        $this->assertDatabaseHas('items', [
-            'id' => $item->id,
-            'name' => 'Updated Item',
-            'amount' => 150,
-            'remaining' => 100,
-        ]);
-    }
-
-    public function test_a_different_user_cannot_edit_a_budget_item()
-    {
-        $user = User::factory()->create();
-        $budget = Budget::factory()->create();
-        $item = Item::factory()->create(['budget_id' => $budget->id]);
-
-        $this->actingAs($user);
-        $response = $this->patch(route('budgets.items.update', ['budget' => $budget->getKey(), 'item' => $item->getKey()]), [
-            'name' => 'Updated Item',
-            'amount' => 150,
-            'remaining' => 100,
-        ]);
-
-        $response->assertForbidden();
-        $this->assertDatabaseMissing('items', [
-            'id' => $item->id,
-            'name' => 'Updated Item',
-            'amount' => 150,
-            'remaining' => 100,
-        ]);
     }
 }
