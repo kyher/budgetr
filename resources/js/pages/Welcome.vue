de380ddd96d118d522534b594046a1656317f0b9
kyher <kyle.heron@pm.me>
2026-03-09 17:37:18 +0000

    Rework welcome page

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -18,36 +18,8 @@ withDefaults(
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
     <div
-        class="items-centerp-6 flex min-h-screen flex-col bg-gradient-to-bl from-[#ffe4e6] to-[#ccfbf1] lg:justify-center lg:p-8"
+        class="items-centerp-6 flex min-h-screen flex-col items-center bg-gradient-to-bl from-[#ffe4e6] to-[#ccfbf1] lg:justify-center lg:p-8"
     >
-        <header
-            class="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl"
-        >
-            <nav class="flex items-center justify-end gap-4">
-                <Link
-                    v-if="$page.props.auth.user"
-                    :href="dashboard()"
-                    class="inline-block rounded-sm bg-blue-500 px-5 py-1.5 text-sm leading-normal text-white hover:bg-blue-600"
-                >
-                    Dashboard
-                </Link>
-                <template v-else>
-                    <Link
-                        :href="login()"
-                        class="inline-block rounded-sm bg-blue-500 px-5 py-1.5 text-sm leading-normal text-white hover:bg-blue-600"
-                    >
-                        Log in
-                    </Link>
-                    <Link
-                        v-if="canRegister"
-                        :href="register()"
-                        class="inline-block rounded-sm bg-blue-500 px-5 py-1.5 text-sm leading-normal text-white hover:bg-blue-600"
-                    >
-                        Register
-                    </Link>
-                </template>
-            </nav>
-        </header>
         <div
             class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"
         >
@@ -56,8 +28,33 @@ withDefaults(
             >
                 <h1 class="text-2xl font-bold">Budgetr</h1>
                 <h2 class="text-lg">
-                    Login or register to manage your budgets
+                    <p v-if="$page.props.auth.user">Welcome back!</p>
+                    <p v-else>Login or register to manage your budgets</p>
                 </h2>
+                <div class="my-2 flex gap-2">
+                    <Link
+                        v-if="$page.props.auth.user"
+                        :href="dashboard()"
+                        class="inline-block rounded-sm bg-blue-500 px-5 py-1.5 text-sm leading-normal text-white hover:bg-blue-600"
+                    >
+                        Dashboard
+                    </Link>
+                    <template v-else>
+                        <Link
+                            :href="login()"
+                            class="inline-block rounded-sm bg-blue-500 px-5 py-1.5 text-sm leading-normal text-white hover:bg-blue-600"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            v-if="canRegister"
+                            :href="register()"
+                            class="inline-block rounded-sm bg-blue-500 px-5 py-1.5 text-sm leading-normal text-white hover:bg-blue-600"
+                        >
+                            Register
+                        </Link>
+                    </template>
+                </div>
             </main>
         </div>
         <div class="hidden h-14.5 lg:block"></div>
